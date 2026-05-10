48980eb7f8c1e19d29452b317b4aac0422aaf1d1
noble <basilgoodluck22@gmail.com>
2026-05-10 05:29:36 -0700

    fix css file import

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../styles.css"
 import { AuthProvider } from "../components/AuthProvider";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
